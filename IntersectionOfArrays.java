de009aaaa856659b55fc4ef4208b033bf3dec24e
navyanair0918 <nairnavya18@gmail.com>
2026-01-04 22:00:28 +0530

    Update IntersectionOfArrays.java

--- IntersectionOfArrays.java
+++ IntersectionOfArrays.java
@@ -7,7 +7,7 @@ class Solution {
         int k=0;
         for(i=0;i<nums1.length;i++)
         {
-            for(j=0;j<nums2.length;j++) //Two for loops for checking each one
+            for(j=0;j<nums2.length;j++) 
             {
                 if(nums1[i]==nums2[j])
                 {
