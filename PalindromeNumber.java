573e445973758c974dd185062b51de8148422bcc
navyanair0918 <nairnavya18@gmail.com>
2026-01-11 22:49:53 +0530

    Refactor isPalindrome method for readability

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) 
     {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) 
+        {
             return false;
         }
 
         int reversedHalf = 0; 
   
-        while (x > reversedHalf) {
+        while (x > reversedHalf) 
+        {
             reversedHalf = reversedHalf * 10 + x % 10;
             x = x / 10;
         }
