570e9df2f745ffecb3ddc47b26537a05b0ad94f9
navyanair0918 <nairnavya18@gmail.com>
2026-01-16 22:45:44 +0530

    Remove unnecessary blank line in code

--- 1266MinimumTimeVisitingAllPoints.java
+++ 1266MinimumTimeVisitingAllPoints.java
@@ -6,8 +6,6 @@ class Solution {
         for (int i = 1; i < points.length; i++) 
         {
             int x1 = points[i - 1][0]; 
-
-            
             int y1 = points[i - 1][1];
             int x2 = points[i][0];
             int y2 = points[i][1];
