181c67d9ea4e207dda8436c5ab4829e24df5c27b
navyanair0918 <nairnavya18@gmail.com>
2026-01-24 23:29:01 +0530

    Update 1266MinimumTimeVisitingAllPoints.java

--- 1266MinimumTimeVisitingAllPoints.java
+++ 1266MinimumTimeVisitingAllPoints.java
@@ -2,9 +2,6 @@ class Solution {
     public int minTimeToVisitAllPoints(int[][] points) 
     {
         int totalTime = 0;
-
-        
-
         for (int i = 1; i < points.length; i++) 
         {
             int x1 = points[i - 1][0]; 
