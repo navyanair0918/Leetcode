75f36b30d43f83ac5705be60c49581809fb6bf90
navyanair0918 <nairnavya18@gmail.com>
2026-01-27 23:08:47 +0530

    Update 1266MinimumTimeVisitingAllPoints.java

--- 1266MinimumTimeVisitingAllPoints.java
+++ 1266MinimumTimeVisitingAllPoints.java
@@ -6,8 +6,10 @@ class Solution {
         {
             int x1 = points[i - 1][0]; 
             int y1 = points[i - 1][1];
+            
             int x2 = points[i][0];
             int y2 = points[i][1];
+            
             int dx = Math.abs(x2 - x1);
             int dy = Math.abs(y2 - y1);
 
