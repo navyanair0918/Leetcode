1da8e4f0fa14ced4b57250fb35d72c6185f58fe6
navyanair0918 <nairnavya18@gmail.com>
2026-02-24 17:45:57 +0000

    Optimal 2sum

--- TwoSum.java
+++ TwoSum.java
@@ -1,16 +1,40 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int i,j;
+//         for(i=0;i<nums.length;i++)
+//         {
+//             for(j=i+1;j<nums.length;j++)
+//             {
+//                 if((nums[i]+nums[j])==target)
+//                 {
+//                     return new int[]{i,j};   //Two for loops
+//                 }
+//             }
+//         }
+//         return new int[]{};        
+//     } 
+// }
+
+
+import java.util.HashMap;
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int i,j;
-        for(i=0;i<nums.length;i++)
+    public int[] twoSum(int[] nums, int target) 
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++)
         {
-            for(j=i+1;j<nums.length;j++)
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement))
             {
-                if((nums[i]+nums[j])==target)
-                {
-                    return new int[]{i,j};   //Two for loops
-                }
+                return new int [] {map.get(complement),i};
             }
+
+            map.put(nums[i],i);
+
         }
-        return new int[]{};        
-    } 
+
+        return new int[]{};
+    
+    }
 }
